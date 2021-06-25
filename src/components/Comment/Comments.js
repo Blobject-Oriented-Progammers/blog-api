9ec7256739a6ec9e76155f1e8e44fe6543533ffd
nayaba <nabila.ayaba@gmail.com>
2021-06-25 15:54:23 -0400

    Comment form with entryId bug
    Have a working comment form, just trying to figure out how to send
    the entryId with the content
    
    Mob coding: JT, Dylon, Nabila

--- src/components/Comment/Comments.js
+++ src/components/Comment/Comments.js
@@ -1,7 +1,65 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react'
+import { Redirect } from 'react-router-dom'
 
-const Comment = ({ content, author }) => (
-  <li>{content} by {author}</li>
-)
+import { createEntry } from '../../api/entries'
+import messages from '../AutoDismissAlert/messages'
 
-export default Comment
+class Comments extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      entry: {
+        title: '',
+        text: ''
+      },
+      createdId: null
+    }
+  }
+
+  handleChange = (event) => {
+    const updatedField = { [event.target.name]: event.target.value }
+    console.log('updatedField in createEntry handleChange: ', updatedField)
+    this.setState((currentState) => {
+      return { entry: {
+        ...currentState.entry,
+        ...updatedField
+      } }
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    const { msgAlert, user } = this.props
+    const entry = { ...this.state.entry, owner: user._id }
+    console.log('this.props, createEntry: ', this.props)
+    createEntry(entry, user)
+      .then(res => this.setState({ createdId: res.data.entry._id }))
+      .then(() => msgAlert({
+        heading: 'Create Entry Success!',
+        message: messages.entryCreateSuccess,
+        variant: 'success'
+      }))
+      .catch(() => msgAlert({
+        heading: 'Create Entry Failed',
+        message: messages.entryCreateFailure,
+        variant: 'danger'
+      }))
+  }
+
+  render () {
+    const { content, author } = this.props
+    if (this.state.createdId) {
+      return <Redirect to={`/entries/${this.state.createdId}`}/>
+    }
+
+    return (
+      <Fragment>
+        <li>{content} by {author}</li>
+      </Fragment>
+    )
+  }
+}
+
+export default Comments
